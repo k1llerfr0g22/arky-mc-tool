3b0f364fa6c65b060454f93e928ab4738364d300
k1llerfr0g22 <hackerfrox@gmail.com>
2021-11-22 07:08:21 +0100

    updated the python <-> java bridge

--- gui_variant/eel_cpp_html_bridge.py
+++ gui_variant/eel_cpp_html_bridge.py
@@ -1,4 +1,5 @@
 import eel
+import threading
 
 @eel.expose
 def hello_world():
@@ -8,6 +9,19 @@ def python_suicide_quit():
 	system("killall python3")
 
 
+def start_server_from_java():
+	
+
+def start_server():
+	# I have to add parameters in this func that define the ram
+	print("start_server worked")
+
+
+# (DEFINE) START SERVR JAR WITH THREADS:
+
+th1 = threading.Thread(target=threadFunc)
+th1.start()
+
 
 eel.init("html")
-eel.start("index.html", cmdline_args=['--kiosk'])
+eel.start("home.html", cmdline_args=['--kiosk'])
