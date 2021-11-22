94ec45e1194989d30013893d92f6f03c6dacf098
k1llerfr0g22 <hackerfrox@gmail.com>
2021-11-22 07:34:18 +0100

    updated command line variant starter

--- start_cmd_line_variant.py
+++ start_cmd_line_variant.py
@@ -1,5 +1,13 @@
 import os
+import time
 
-os.system("cd cmd_line_variant")
-os.system("g++ main.cpp -o main.bin")
-os.system(./main.bin)
+print("starting the mc-server-tool in command line variant")
+time.sleep(1)
+print("compiling code...")
+os.system("cd /home/k1llerfr0g/code/c++/projects/tools/mc-server-tool/cmd_line_variant && g++ main.cpp -o main.bin")
+print("DONE!")
+time.sleep(0.8)
+os.system("clear")
+print("PRESS ENTER TO START")
+input()
+os.system("cd /home/k1llerfr0g/code/c++/projects/tools/mc-server-tool/cmd_line_variant && ./main.bin")
