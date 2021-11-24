3f37d8eba83a2ce05acf857b4e0ab07e94a002e4
k1llerfr0g22 <hackerfrox@gmail.com>
2021-11-24 07:27:37 +0100

    updated python server starter / bridge

--- gui_variant/eel_cpp_html_bridge.py
+++ gui_variant/eel_cpp_html_bridge.py
@@ -1,5 +1,8 @@
 import eel
 import threading
+import os
+
+six = "-Xmx6g"
 
 @eel.expose
 def hello_world():
@@ -9,19 +12,37 @@ def python_suicide_quit():
 	system("killall python3")
 
 
-def start_server_from_java():
-	
+#def start_server_from_js():
 
-def start_server():
-	# I have to add parameters in this func that define the ram
+ram = input("input_ram: ")
+
+if ram == "2":
+	th1 = threading.Thread(target=start_server, args=(2,))
+
+elif ram == "4":
+	th1 = threading.Thread(target=start_server, args=(4,))
+
+elif ram == "6":
+	th1 = threading.Thread(target=start_server, args=(6,))
+
+
+def start_server(ram):
+
+	if ram == 6:
+		ram =
+	os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(six) + " -jar minecraft_server1.17.1.jar")
 	print("start_server worked")
 
 
+def start_html():
+	eel.init("html")
+	eel.start("home.html", cmdline_args=['--kiosk'])
+
 # (DEFINE) START SERVR JAR WITH THREADS:
 
-th1 = threading.Thread(target=threadFunc)
+start_html = threading.Thread(target=start_html)
+th1 = threading.Thread(target=start_server, args=(4,))
 th1.start()
-
-
-eel.init("html")
-eel.start("home.html", cmdline_args=['--kiosk'])
+start_html.start()
+th1.join()
+start_html.join()
