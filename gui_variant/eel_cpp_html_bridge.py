295f2461eacb239e856ad356af850150023dcaa1
k1llerfr0g22 <hackerfrox@gmail.com>
2021-11-30 20:26:18 +0100

    hell yeah this sucks and nothing works

--- gui_variant/eel_cpp_html_bridge.py
+++ gui_variant/eel_cpp_html_bridge.py
@@ -1,1 +1,69 @@
-test
+import eel
+import threading
+import os
+
+#### VARS:      ####
+# VARS FOR RAM AMOUNT ARG 	
+two = "-Xmx2g"
+four = "-Xmx4g"
+six = "-Xmx6g"
+
+
+#### FUNCTIONS: ####
+@eel.expose
+
+def hello_world():
+	print("hello world")
+
+def python_suicide_quit():
+	system("killall python3")
+
+
+#def start_server_from_js(): (idk why this is here but maybe ill need this reminder later...)
+
+def start_server(ram):
+
+	if ram == 6:
+		os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(six) + " -jar minecraft_server1.17.1.jar")
+		print("start_server worked")
+
+	if ram == 4:
+		os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(four) + " -jar minecraft_server1.17.1.jar")
+		print("start_server worked")
+
+	if ram == 2:
+		os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(two) + " -jar minecraft_server1.17.1.jar")
+		print("start_server worked")
+
+
+def start_html():
+	eel.init("html")
+	eel.start("home.html", cmdline_args=['--kiosk'])
+
+# DEFINE THREADS:
+
+th1 = threading.Thread(target=start_server, args=(6,))
+start_html =  threading.Thread(target=start_server)
+
+ram = input("input ram:")
+
+
+# ACTUAL RUNNING SCRIPT (OR IDK HOW TO CALL IT LOL)
+while True:
+	server_running = True
+	if (server_running == True) and (ram == 6):
+		th1.start()
+		th1.join()
+
+
+
+start_html.start()
+start_html.join()
+
+
+
+
+
+
+
+
