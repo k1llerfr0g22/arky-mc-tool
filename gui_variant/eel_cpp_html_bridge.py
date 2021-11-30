40863ae2811ee19532b70522cb79501d9b8c45fb
k1llerfr0g22 <hackerfrox@gmail.com>
2021-11-30 19:28:03 +0100

    updated bridge (um... and it didnt works) f

--- gui_variant/eel_cpp_html_bridge.py
+++ gui_variant/eel_cpp_html_bridge.py
@@ -2,9 +2,16 @@ import eel
 import threading
 import os
 
+#### VARS:      ####
+# VARS FOR RAM AMOUNT ARG 	
+two = "-Xmx2g"
+four = "-Xmx4g"
 six = "-Xmx6g"
 
+
+#### FUNCTIONS: ####
 @eel.expose
+
 def hello_world():
 	print("hello world")
 
@@ -12,26 +19,21 @@ def python_suicide_quit():
 	system("killall python3")
 
 
-#def start_server_from_js():
-
-ram = input("input_ram: ")
-
-if ram == "2":
-	th1 = threading.Thread(target=start_server, args=(2,))
-
-elif ram == "4":
-	th1 = threading.Thread(target=start_server, args=(4,))
-
-elif ram == "6":
-	th1 = threading.Thread(target=start_server, args=(6,))
-
+#def start_server_from_js(): (idk why this is here but maybe ill need this reminder later...)
 
 def start_server(ram):
 
 	if ram == 6:
-		ram =
-	os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(six) + " -jar minecraft_server1.17.1.jar")
-	print("start_server worked")
+		os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(six) + " -jar minecraft_server1.17.1.jar")
+		print("start_server worked")
+
+	if ram == 4:
+		os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(four) + " -jar minecraft_server1.17.1.jar")
+		print("start_server worked")
+
+	if ram == 2:
+		os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(two) + " -jar minecraft_server1.17.1.jar")
+		print("start_server worked")
 
 
 def start_html():
@@ -40,9 +42,26 @@ def start_html():
 
 # (DEFINE) START SERVR JAR WITH THREADS:
 
+
+
+
 start_html = threading.Thread(target=start_html)
-th1 = threading.Thread(target=start_server, args=(4,))
-th1.start()
+th1 = threading.Thread(target=start_server, args=(2,))
+th2 = threading.Thread(target=start_server, args=(4,))
+th3= threading.Thread(target=start_server, args=(6,))
+
+
+# MAIN SCRIPT
+
+ram = input("input_ram: ")
+
 start_html.start()
-th1.join()
 start_html.join()
+t1.start()
+t1.join()
+
+
+
+
+
+
