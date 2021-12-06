e8b211b3f4a9701694815cefb0c1b0acfc6fe099
k1llerfr0g22 <hackerfrox@gmail.com>
2021-12-06 07:05:19 +0100

    made merge but with a really messy messy method i guess lol

--- gui_variant/eel_cpp_html_bridge.py
+++ gui_variant/eel_cpp_html_bridge.py
@@ -1,28 +1,32 @@
+#!/usr/bin/python
 import eel
 import threading
 import os
+import time
 
 #### VARS:      ####
 # VARS FOR RAM AMOUNT ARG 	
 two = "-Xmx2g"
 four = "-Xmx4g"
 six = "-Xmx6g"
+start_server_bool = True
+
+
+# START MSG:
+print("starting")
 
 
 #### FUNCTIONS: ####
 @eel.expose
-
-def hello_world():
-	print("hello world")
-
 def python_suicide_quit():
 	system("killall python3")
 
 
+
 #def start_server_from_js(): (idk why this is here but maybe ill need this reminder later...)
 
-def start_server(ram):
-
+"""def start_server():
+	global ram
 	if ram == 6:
 		os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(six) + " -jar minecraft_server1.17.1.jar")
 		print("start_server worked")
@@ -33,32 +37,51 @@ def start_server(ram):
 
 	if ram == 2:
 		os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(two) + " -jar minecraft_server1.17.1.jar")
-		print("start_server worked")
+		print("start_server worked")"""
+
+def loading(loading_step):
+	if loading_step == 1:
+		print("   loading " + "|" + u'\u2588' +  u'\u2593' + u'\u2592' + u'\u2591');
+
+
+
+
+
+def start_server_2g():
+	print("starting")#os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(two) + " -jar minecraft_server1.17.1.jar")
+
+def start_server_4g():
+	os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(four) + " -jar minecraft_server1.17.1.jar")
+
+def start_server_6g():
+	os.system("cd /home/k1llerfr0g/code/server/minecraft/aternos && java " + str(six) + " -jar minecraft_server1.17.1.jar")
 
 
 def start_html():
 	eel.init("html")
 	eel.start("home.html", cmdline_args=['--kiosk'])
 
-# (DEFINE) START SERVR JAR WITH THREADS:
+
+while True:
+	loading(1)
+	loading(2)
 
 
-
+#### THREADS:   ####
 
 start_html = threading.Thread(target=start_html)
-th1 = threading.Thread(target=start_server, args=(2,))
-th2 = threading.Thread(target=start_server, args=(4,))
-th3= threading.Thread(target=start_server, args=(6,))
+sts2g = threading.Thread(target=start_server_2g)
+sts4g = threading.Thread(target=start_server_4g)
+sts2g = threading.Thread(target=start_server_6g)
 
-
-# MAIN SCRIPT
-
-ram = input("input_ram: ")
-
+sts2g.start()
 start_html.start()
+
+
+sts2g.join()
 start_html.join()
-t1.start()
-t1.join()
+
+
 
 
 
