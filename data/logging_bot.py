91194a6919447e495db649da2f7dd73b76c029c9
k1llerfr0g22 <hackerfrox@gmail.com>
2021-12-24 18:27:41 +0100

    updated bot

--- data/logging_bot.py
+++ data/logging_bot.py
@@ -5,17 +5,20 @@ now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 
-def log(event):
+def log(event, embed_msg):
+	now = datetime.now()
+	dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 	time = "[ " + dt_string + " ]:"
 
-	webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/923976550410485791/iqDWmTmpXZf37_J7nPpZADqIEc_6Ew9kJo0LI5PDwaf7HC6O1tVZalJjcKs9TCrE5tgt", username="LOGGING BOT", content="", avatar_url="http://cdn.onlinewebfonts.com/svg/img_543604.png")
+	webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/923976550410485791/iqDWmTmpXZf37_J7nPpZADqIEc_6Ew9kJo0LI5PDwaf7HC6O1tVZalJjcKs9TCrE5tgt", username="LOGGING BOT", content=time, avatar_url="http://cdn.onlinewebfonts.com/svg/img_543604.png")
+	
+	#time_embed = DiscordEmbed(title="***" + time + "***", color=242424) # define embed
+	#webhook.add_embed(time_embed) # add embed time
 
-	time_embed = DiscordEmbed(title="***" + time + "***", color=242424) # define embed
-	log = DiscordEmbed(title="***" + event + "***", color=242424) # define embed
-	webhook.add_embed(time_embed) # add embed var
-	webhook.add_embed(log) # add embed var
 
+	embed = DiscordEmbed(title="***" + embed_msg + "***", color=242424) # define embed
+	webhook.add_embed(embed)
 
 	response = webhook.execute() # do whatever
 
-log("ehehehehe")
+log("test", "nothing") #test
